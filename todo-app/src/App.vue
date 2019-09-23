d15f0f6800b351f9328cc3cdb2456c7c9c6b32ae
Giang Nguyen <thuygiang152@gmail.com>
2019-09-23 17:28:35 +0300

    Sending HTTP request in VueJS with axios

--- todo-app/src/App.vue
+++ todo-app/src/App.vue
@@ -7,6 +7,7 @@
 </template>
 
 <script>
+import axios from "axios";
 import Header from "./components/layout/Header";
 import AddTodo from "./components/AddTodo";
 import Todos from "./components/Todos";
@@ -26,9 +27,22 @@ export default {
     removeTodo(id) {
       this.todos = this.todos.filter(todo => todo.id != id);
     },
-    addTodo(todo) {
-      this.todos = [...this.todos, todo];
+    addTodo(newTodo) {
+      const { title, completed } = newTodo;
+      axios
+        .post("https://jsonplaceholder.typicode.com/todos", {
+          title,
+          completed
+        })
+        .then(res => (this.todos = [...this.todos, res.data]))
+        .catch(err => console.log(err));
     }
+  },
+  created() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/todos")
+      .then(res => (this.todos = res.data))
+      .catch(err => console.log(err));
   }
 };
 </script>
