3e862c877711fcccbd8476dc013a82a0df43d293
Giang Nguyen <thuygiang152@gmail.com>
2019-09-23 18:43:17 +0300

    Editing About page

--- todo-app/src/views/About.vue
+++ todo-app/src/views/About.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About the app</h1>
+    <p>
+      This is a very basic, minimal todo-app built with VueJS framework by Giang Nguyen in September 2019.
+      The idea is to get familiarized with the framework.
+    </p>
+
+    <p>Contact me at <a id="email" href="mailto: giangster.nguyen@gmail.com">giangster.nguyen@gmail.com</a> if you have any questions. 
+    Even if you don't have one... it's okay, send me a cat/ dog video and we will be friends.</p>
   </div>
 </template>
+
+<style>
+#email {
+  text-decoration: none;
+  color: darkblue;
+}
+</style>
